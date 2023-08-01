260a103c16408226acab1a8c8daa69738b918607
Daesick <dsryu0822@gmail.com>
2023-08-01 17:05:26 +0900

    17.126 ms (32 allocations: 23.31 MiB)   17.049 ms (16 allocations: 23.27 MiB)

--- src/ML.jl
+++ src/ML.jl
@@ -14,3 +14,54 @@ end
 # using Plots
 # scatter(eachcol(Y)..., msw = 0, color = dbs.assignments, label = :none)
 
+using Flux
+struct Fourier0
+    an
+    bn
+    L
+end
+Fourier0(m::Integer) = Fourier0(randn(Float32, m), randn(Float32, m), Float32[0.0])
+function (layer::Fourier0)(x)
+    m = length(layer.an)
+    nxL⁻¹ = (1:m) * x[end, :]' .* exp(layer.L[1])
+    return [x[1:end-1, :]; sum(
+        [cospi.(nxL⁻¹) .* layer.an
+       ; sinpi.(nxL⁻¹) .* layer.bn], dims = 1)]
+end
+Flux.@functor Fourier0
+
+function Base.show(io::IO, l::Fourier0)
+    print(io, "Fourier(", length(l.an), ")")
+end
+# ------------------------------
+
+struct Fourier
+    m::Int64
+    an::Array{Float32, 1}
+    bn::Array{Float32, 1}
+    L::Float32
+end
+function Base.show(io::IO, l::Fourier)
+    print(io, "Fourier(", l.m, ")")
+end
+Fourier(m::Integer) = Fourier(m, randn(Float32, m), randn(Float32, m), zero(Float32))
+
+function (layer::Fourier)(x)
+    nxL⁻¹ = (1:layer.m) .* last(eachrow(x))' * exp(layer.L)
+    return [x[1:end-1, :]; sum(
+        [cospi.(nxL⁻¹) .* layer.an
+       ; sinpi.(nxL⁻¹) .* layer.bn], dims = 1)]
+
+    # nxL⁻¹ = (1:layer.m) .* last(eachrow(x))' * exp(layer.L)
+    # return [x[1:end-1, :]; sum([sinpi.(nxL⁻¹); cospi.(nxL⁻¹)] .* layer.coeff, dims = 1)]
+end
+Flux.@functor Fourier
+
+x = rand(Float32, 5, 10000)
+
+F0 = Fourier0(100)
+F = Fourier(100)
+
+using BenchmarkTools
+@btime F0(x);
+@btime F(x);
