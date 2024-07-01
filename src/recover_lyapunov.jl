a21423c6deb0410e8a4720d5d07a2e46be8e4d25
dsryu0822 <rmsmsgood@naver.com>
2024-07-01 11:46:03 +0900

    deploy server

--- src/recover_lyapunov.jl
+++ src/recover_lyapunov.jl
@@ -6,8 +6,10 @@ include("../core/visual.jl")
 
 if Sys.iswindows()
     cd("G:/DDM")
+    device = ENV["COMPUTERNAME"]
 elseif Sys.islinux()
     cd("/home/$(ENV["LOGNAME"])/g/DDM")
+    device = ENV["HOSTNAME"]
 end
 
 # canonicalize(2001*100*50sec)
@@ -30,8 +32,6 @@ end
 #      -π*sinpi(t) ifelse(abs(u) ≥ d/2, -160000, 0) ifelse(abs(u) ≥ d/2, -172.363, 0)
 # ]
 
-# print(f_[1])
-# print(f_[2])
 function lyapunov_soft()
     schedules = CSV.read("lyapunov/soft_schedules_cache.csv", DataFrame)
     vrbl = [:dt, :du, :dv], [:t, :u, :v]
@@ -39,12 +39,11 @@ function lyapunov_soft()
     dt = 1e-6; θ1 = 1e-8; θ2 = 1e-12; θ3 = 1e-5; min_rank = 21;
 
     sync = 0
-    # for dr = eachrow(schedules)[[1563]]
     # chaos1    -    1:500
     # chaos2    -  501:1000
     # chaos3    - 1001:1500
     # sicklinux - 1501:end 
-    @showprogress @threads for dr = eachrow(schedules)[1:end]
+    @showprogress @threads for dr = eachrow(schedules)[1:1000]
         # dr = eachrow(schedules)[1563]
         
         filename = "lyapunov/soft/$(lpad(dr.idx, 5, '0')).csv"
@@ -61,8 +60,6 @@ function lyapunov_soft()
 
         try
             if sync ≤ nthreads()
-                # sleep(dr.idx/10); sync += 1 # It's technical trick for threading
-                # sleep(nthreads()*rand()) # It's technical trick for threading
                 sleep(dr.idx % 109) # It's technical trick for threading
                 J_ = jacobian.(f_)
             end
@@ -74,86 +71,31 @@ function lyapunov_soft()
         
         s = apply_tree(Dtree, collect(note[end, 3:5]))
         J = substitute(J_[s], Dict(t => note.t[end]))
-        # J = J_soft(collect(note[1, 3:5])..., dr.d)
         
         U = reshape(collect(note[end, 6:(end-3)]), 3, 3)
         if U |> isone
             U, _ = qr(J); U = Matrix(U)
         end
-        @info "$(now()) lyapunov start"
         for _t = round(Int64, note.t[end]):10:40
             
             data = DataFrame(solve(f_, collect(note[end, 3:5]), dt, _t:dt:(_t+10), Dtree), last(vrbl))
-            # λ = collect(note[end, ((end-2):end)])
-            # x = collect(note[end, 3:5])
             λ = zeros(3)
             for i in 1:nrow(data)
                 U, V = gram_schmidt(U)
                 λ += V |> eachcol .|> norm .|> log
                 
                 U = RK4(J, U, dt)
-                # s = abs(data.u[i]) ≥ dr.d/2 ? 2 : 1
                 s = apply_tree(Dtree, collect(data[i, 1:3]))
                 J = Float64.(substitute(J_[s], Dict(t => data.t[i])))
-                # 이게 더 중요한 것 같은데??
-
-                # s = apply_tree(Dtree, x)
-                # x, _ = RK4(f_[s], x, dt)
-                # J = substitute(J_[s], Dict(t => x[1]))
-                # s = apply_tree(Dtree, x)
-                # J = Float64.(substitute(J_[s], Dict(t => x[1])))
-                # x, _ = RK4(f_[s], x, dt)
             end; λ
-
-            #############################
-            # data = factory_soft(DataFrame, dr.idx, dr.d; ic = collect(note[end, 3:5]), tspan = [_t, _t+10], dt)[:, 1:3]
-            # λ = zeros(3);
-            # for i in 1:nrow(data)
-            #     U, V = gram_schmidt(U)
-            #     λ += V |> eachcol .|> norm .|> log
-
-            #     U = RK4(J, U, dt)
-            #     J = J_soft(data.t[i], data.u[i], data.v[i], dr.d)
-            # end; λ
-            #############################
 
             push!(note, [dr.idx, dr.d, data[end, 1:3]..., U..., λ...])
             CSV.write(filename, note, bom = true)
             CSV.write(string(@__DIR__) * "/../archive/$(lpad(dr.idx, 5, '0')).csv", note, bom = true)
         end
     end
-    open("lyapunov/$(ENV["COMPUTERNAME"]) done!", "w") do io
+    open("lyapunov/$(device) done!", "w") do io
         println(io, Dates.now())
     end
 end
 lyapunov_soft()
-
-# plot(_data.t[1:1000:end], _data.v[1:1000:end]); png("temp1")
-# plot( data.t[1:1000:end],  data.v[1:1000:end]); png("temp2")
-
-
-# X = [0	-0.464422967	-0.885613985;	0	-0.88561352	0.464422082;	1	5.52e-17	1.10e-10]
-# Y = [0	-0.493610779	-0.869683424;	0	-0.86968293	0.49360991;	1	9.16E-19	1.83E-12]
-
-# qr(X).R |> eachrow .|> norm .|> log
-# qr(Y).R |> eachrow .|> norm .|> log
-
-# _λ
-# λ
-# findall(s1 .!= s2)
-
-# function prepare_soft()
-#     schedules = CSV.read("lyapunov/soft_schedules_cache.csv", DataFrame)
-#     vrbl = [:dt, :du, :dv], [:t, :u, :v]
-#     cnfg = (; f_ = [cospi, sign], λ = 1e-2)
-#     dt = 1e-6; θ1 = 1e-8; θ2 = 1e-12; θ3 = 1e-5; min_rank = 21;
-
-#     @showprogress @threads for dr = eachrow(schedules)
-#         filename = "lyapunov/soft/$(lpad(dr.idx, 5, '0')).csv"
-#         note = CSV.read(filename, DataFrame); note[!, Not(1)] .= Float64.(note[!, Not(1)])
-#         data = factory_soft(DataFrame, dr.idx, dr.d; ic = collect(note[end, 3:5]), tspan = [0, 10], dt)
-#         add_subsystem!(data, vrbl, cnfg; (; θ1, θ2, θ3, min_rank)...); # 30 sec
-#         CSV.write("G:/DDM/lyapunov/big/$(lpad(dr.idx, 5, '0')).csv", data, bom = true)
-#     end
-# end
-# prepare_soft()
--- test/monitor.jl
+++ test/monitor.jl
@@ -13,22 +13,41 @@ cd("G:/DDM/lyapunov")
 scatter(nrow.(files))
 histogram(nrow.(files))
 
-
+lpnv1, lpnv2, lpnv3 = [], [], []
+for k = 1:2001
+    _t = files[k].t[end]
+    temp = [sum(files[k].λ1) ./ _t, 
+            sum(files[k].λ2) ./ _t, 
+            sum(files[k].λ3) ./ _t]
+    sort!(temp)
+    push!(lpnv1, temp[1])
+    push!(lpnv2, temp[2])
+    push!(lpnv3, temp[3])
+end
+plot(size = [600, 300], legend = :none)
+plot!(.1:.0001:.3, lpnv1, color = 1, ms = 1)
+plot!(.1:.0001:.3, lpnv2, color = 2, ms = 1)
+plot!(.1:.0001:.3, lpnv3, color = 3, ms = 1)
+png("temp")
+findall(lpnv1 .> -0.5)
 # completed = eachrow.(files[nrow.(files) .== 6]);
 completed = eachrow.(files);
+
 temp = stack(getindex.(completed, nrow.(completed)))'
 temp = stack(getindex.(completed, 6))'
-temp[:, 6:8] ./= (temp[:, 3] .- 50)
+temp[:, (end-2):end] ./= temp[:, 3]
 for i in axes(temp, 1)
-    temp[i, 6:8] .= sort(temp[i, 6:8])
+    temp[i, (end-2):end] .= sort(temp[i, (end-2):end])
 end
 
 scatter(temp[:, end])
 scatter(temp[:, end], xlims = [1545, 1550], xticks = 1545:1550)
 
 plot(size = [600, 300], legend = :none)
-plot!(temp[:, 6], color = 1, ms = 1)
-plot!(temp[:, 7], color = 2, ms = 1)
-plot!(temp[:, 8], color = 3, ms = 1)
+plot!(temp[:, end-2], color = 1, ms = 1)
+plot!(temp[:, end-1], color = 2, ms = 1)
+plot!(temp[:, end-0], color = 3, ms = 1)
 plot!(xlims = [1560, 1570], xticks = 1560:1570)
 png("temp")
+
+
