1daf9bd19b0f7728219260f79f86afee4dfccd0a
dsryu0822 <rmsmsgood@naver.com>
2024-09-10 16:25:19 +0900

    server deploy

--- src/recover_lyapunov.jl
+++ src/recover_lyapunov.jl
@@ -19,41 +19,40 @@ function lyapunov_exponent(_data::DataFrame, J_::Vector{Matrix{Num}}, DT::Root{F
     return sort(λ / T, rev=true)
 end
 
-##########################################################################
-#                                                                        #
-#                            Soft impact model                           #
-#                                                                        #
-##########################################################################
-schedules = CSV.read("bifurcation/soft_schedules.csv", DataFrame)[1:10:end, :]
-schedules[!, :λ1] .= .0; schedules[!, :λ2] .= .0; schedules[!, :λ3] .= .0;
-vrbl = [:dt, :du, :dv], [:t, :u, :v]
-cnfg = (; f_ = [cospi, sign], λ = 1e-2)
-dt = 1e-6; θ1 = 1e-8; θ2 = 1e-12; θ3 = 1e-5; min_rank = 21;
+# ##########################################################################
+# #                                                                        #
+# #                            Soft impact model                           #
+# #                                                                        #
+# ##########################################################################
+# schedules = CSV.read("bifurcation/soft_schedules.csv", DataFrame)[1:10:end, :]
+# schedules[!, :λ1] .= .0; schedules[!, :λ2] .= .0; schedules[!, :λ3] .= .0;
+# vrbl = [:dt, :du, :dv], [:t, :u, :v]
+# cnfg = (; f_ = [cospi, sign], λ = 1e-2)
+# dt = 1e-6; θ1 = 1e-8; θ2 = 1e-12; θ3 = 1e-5; min_rank = 21;
 
-@showprogress @threads for dr = eachrow(schedules)[iszero.(schedules.λ1)]
 # @showprogress @threads for dr = eachrow(schedules)
-    filename = "lyapunov/soft_traj/$(lpad(dr.idx, 5, '0')).csv"
-    data = CSV.read(filename, DataFrame)
+#     filename = "lyapunov/soft_traj/$(lpad(dr.idx, 5, '0')).csv"
+#     data = CSV.read(filename, DataFrame)
     
-    # add_subsystem!(data, vrbl, cnfg; θ1, θ2, θ3, min_rank); # 30 sec
-    f_ = [SINDy(df, vrbl...; cnfg...) for df in groupby(data, :subsystem)]
-    Dtree = dryad(data, last(vrbl)); # print_tree(Dtree)
-    J_ = []
-    while true
-        try
-            J_ = jacobian.(f_)
-            break
-        catch
-            print(".")
-        end
-    end
+#     # add_subsystem!(data, vrbl, cnfg; θ1, θ2, θ3, min_rank); # 30 sec
+#     f_ = [SINDy(df, vrbl...; cnfg...) for df in groupby(data, :subsystem)]
+#     Dtree = dryad(data, last(vrbl)); # print_tree(Dtree)
+#     J_ = []
+#     while true
+#         try
+#             J_ = jacobian.(f_)
+#             break
+#         catch
+#             print(".")
+#         end
+#     end
 
-    data = DataFrame(solve(f_, [eps(), .05853, .47898], dt, 0:dt:150, Dtree), last(vrbl))
-    λ = lyapunov_exponent(data[:, last(vrbl)], J_, Dtree, dr.bp)
-    dr[[:λ1, :λ2, :λ3]] .= λ
-    CSV.write("lyapunov/!$(device)ing soft_lyapunov_rcvd.csv", schedules, bom = true)
-end
-CSV.write("lyapunov/!$(device) soft_lyapunov_rcvd.csv", schedules, bom = true)
+#     data = DataFrame(solve(f_, [eps(), .05853, .47898], dt, 0:dt:150, Dtree), last(vrbl))
+#     λ = lyapunov_exponent(data[:, last(vrbl)], J_, Dtree, dr.bp)
+#     dr[[:λ1, :λ2, :λ3]] .= λ
+#     CSV.write("lyapunov/!$(device)ing soft_lyapunov_rcvd.csv", schedules, bom = true)
+# end
+# CSV.write("lyapunov/!$(device) soft_lyapunov_rcvd.csv", schedules, bom = true)
 
 
 # ##########################################################################
@@ -112,3 +111,54 @@ CSV.write("lyapunov/!$(device) soft_lyapunov_rcvd.csv", schedules, bom = true)
 # end
 # CSV.write("lyapunov/6채우기 hrnm_lyapunov.csv", schedules, bom = true)
 # CSV.write("lyapunov/!$(device) hrnm_lyapunov.csv", schedules, bom = true)
+
+##########################################################################
+#                                                                        #
+#                             Gear system                                #
+#                                                                        #
+##########################################################################
+# data = factory_gear(DataFrame, -.09; dt = 1e-2, tspan = [500, 5000])
+# plot(data.x, data.v)
+# idx_sampled = diff([0; mod.(data.θ, 2π)]) .< 0
+# data = data[idx_sampled, :]
+# scatter(data.x, data.v, ms = 0.5, legend = :none)
+schedules = CSV.read("bifurcation/gear_schedules.csv", DataFrame)
+schedules[!, :λ1] .= .0; schedules[!, :λ2] .= .0; schedules[!, :λ3] .= .0; schedules[!, :λ4] .= .0;
+vrbl = [:dx, :dv, :dΩ, :dθ], [:x, :v, :Ω, :θ]
+cnfg = (; N = 1, f_ = [cos], C = 2,  λ = 1e-2)
+dt = 1e-2; tend = 10000; θ1 = 7e-9; θ2 = 1e-12; θ3 = 1e-5; min_rank = 31; dos = 1
+
+bfcn = DataFrame(hrzn = [], vrtc = [])
+@showprogress @threads for dr = eachrow(schedules)
+    filename = "bifurcation/gear/$(lpad(dr.idx, 5, '0')).csv"
+    data = CSV.read(filename, DataFrame)
+    
+    # add_subsystem!(data, vrbl, cnfg; θ1, θ2, θ3, min_rank); # 30 sec
+    f_ = [SINDy(df, vrbl...; cnfg...) for df in groupby(data, :subsystem)]
+    Dtree = dryad(data, last(vrbl)); # print_tree(Dtree)
+    J_ = []
+    while true
+        try
+            J_ = jacobian.(f_)
+            break
+        catch
+            print(".")
+        end
+    end
+
+    data = DataFrame(solve(f_, [0.1, 0.1, 0.1, eps()], dt, 0:dt:tend, Dtree), last(vrbl))
+    λ = lyapunov_exponent(data[:, last(vrbl)], J_, Dtree, dr.bp, T = tend)
+    dr[[:λ1, :λ2, :λ3, :λ4]] .= λ
+    CSV.write("lyapunov/!$(device)ing gear_lyapunov_rcvd.csv", schedules, bom = true)
+    
+    data = data[data.Ω .> 5000, :]
+    idx_sampled = diff([0; mod.(data.Ω, 2π)]) .< 0
+    sampledx = data.v[idx_sampled]
+    hrzn, vrtc = fill(dr.bp, length(sampledx)), sampledx
+    append!(bfcn, DataFrame(; hrzn, vrtc))
+    
+    CSV.write("lyapunov/!$(device)ing gear_lyapunov_rcvd.csv", schedules, bom = true)
+    CSV.write("lyapunov/!$(device)ing gear_bifurcation_rcvd.csv", bfcn, bom = true)
+end
+CSV.write("lyapunov/!$(device) gear_lyapunov_rcvd.csv", schedules, bom = true)
+CSV.write("lyapunov/!$(device) gear_bifurcation_rcvd.csv", bfcn, bom = true)
