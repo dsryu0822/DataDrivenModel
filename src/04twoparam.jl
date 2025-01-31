b4caf785be1efcdb6a224b967fc3bfd5926eeaf4
dsryu0822 <rmsmsgood@naver.com>
2025-01-31 12:30:01 +0900

    server deploy

--- src/04twoparam.jl
+++ src/04twoparam.jl
@@ -72,19 +72,20 @@ bp1, bp2 = sort([0.100, 0.101]);
 data_ = [factory_(sysname)(DataFrame, bp1; tspan, dt),
          factory_(sysname)(DataFrame, bp2; tspan, dt)]
 for data in data_ add_subsystem!(data, vrbl, cnfg; θ) end
+replace!(data_[2].subsystem, 2 => 3, 3 => 2)
 
-# replace!(data_[2].subsystem, 2 => 3, 3 => 2)
 f__ = [[SINDy(df, vrbl...; cnfg...) for df in groupby(data, :subsystem)] for data in data_]
 _cnfg_ = [getproperty.(Ref(f_), f_ |> propertynames) for f_ in f__[1]];
 Dtree_ = [dryad(data, last(vrbl)) for data in data_] # print_tree.(Dtree_)
-df_Dtree_ = dt2df.(Dtree_)
-# jacobian.(Matrix, f__[1])[2]
+df_Dtree_ = dt2df.(Dtree_);
+for df_Dtree in df_Dtree_ df_Dtree.subsystem .= [3, 1, 1, 2] end
+
+# jacobian.(Matrix, f__[1])[3]
 J_ = [
     vv -> [0 0 0; 0 0 1; -π*sinpi(vv[1]) 0 0],
     vv -> [0 0 0; 0 0 1; -π*sinpi(vv[1]) -160000 -172.363],
     vv -> [0 0 0; 0 0 1; -π*sinpi(vv[1]) -160000 -172.363],
 ]
-
 M1 = Matrix(df_Dtree_[1][:, 1:(end-1)])
 M2 = Matrix(df_Dtree_[2][:, 1:(end-1)])
 
@@ -99,9 +100,7 @@ vrtc = [Float64[] for _ in _idx]
 
     M0 = DataFrame(wsum(M1, M2, pin), last(vrbl))
     M0.subsystem = df_Dtree_[2].subsystem
-    Dtree = dryad(M0, last(vrbl))
-    # print_tree(Dtree)
-    # apply_tree(Dtree, [.0, 1, 0])
+    Dtree = dryad(M0, last(vrbl)) # print_tree(Dtree)
 
     f_ = []
     for (f_1, f_2, _cnfg) = collect(zip(f__[1], f__[2], _cnfg_))
@@ -111,7 +110,7 @@ vrtc = [Float64[] for _ in _idx]
         push!(f_, STLSQresult(__cnfg...))
     end
     # J_ = []; while true try J_ = jacobian.(Function, f_); break; catch; print("."); end end
-    data = DataFrame(solve(f_, [eps(), .05853, .47898], dt, 0:dt:10, Dtree), last(vrbl))
+    data = DataFrame(solve(f_, [eps(), .05853, .47898], dt, 0:dt:100, Dtree), last(vrbl))
     data = data[(nrow(data) ÷ 2):end, :]
 
     λ = lyapunov_exponent(data[:, last(vrbl)], J_, Dtree, dr.bp)
@@ -134,4 +133,4 @@ CSV.write("!$(device) $(sysname)_bfcn_rcvd.csv", bfcn, bom = true)
 
 sysname = "gear" # Fe ∈ [-0.2, 0.2]
 sysname = "hrnm" # l ∈ [0, 1]
-# plot(data.u[1:100:1000000])
+# plot(data.u[1:100:end])
